95e92f79878afc5c356655b04279e69226cb50e2
horobox <horometer@protonmail.com>
2020-03-15 08:45:28 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def select():
+    if request.method == 'POST':
+        semester = request.form['semester']
+        return render_template('sgpa.html', value=semester)
     return render_template('base.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
