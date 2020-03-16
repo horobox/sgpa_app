a17e5fefc85b6fb71ca206381a9079c2d5fc9e67
horobox <horometer@protonmail.com>
2020-03-17 00:27:15 +0530

    Added marks inputing feature v.1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import json
 app = Flask(__name__)
 
@@ -9,10 +9,20 @@ with open('course_list.json') as f:
 def select():
     if request.method == 'POST':
         semester = request.form['semester']
-        if semester in data:
-            Key = data[semester]
-            return render_template('input.html', Key=Key)
+        return redirect(url_for('input_marks', semester=semester))
+    
     return render_template('base.html')
+
+
+@app.route('/<semester>', methods=['GET', 'POST'])
+def input_marks(semester):
+    if semester in data:
+        Key = data[semester]
+
+    if request.method == 'POST':
+        marks = request.form
+        return render_template('result.html', marks=marks)
+    return render_template('input.html', Key=Key)
 
 if __name__ == "__main__":
 	app.run(debug=True)
